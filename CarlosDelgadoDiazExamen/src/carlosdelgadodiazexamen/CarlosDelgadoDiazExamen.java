da9326a0777205c482c9f230c6407fcd76a17eb6
Carlos Delgado <daw202110@estudiantes.salesianasnsp.es>
2021-04-06 12:02:39 +0200

    Esta es la práctica del exámen de recuperación editada2

--- CarlosDelgadoDiazExamen/src/carlosdelgadodiazexamen/CarlosDelgadoDiazExamen.java
+++ CarlosDelgadoDiazExamen/src/carlosdelgadodiazexamen/CarlosDelgadoDiazExamen.java
@@ -18,7 +18,7 @@ public class CarlosDelgadoDiazExamen {
         // TODO code application logic here
     }
     
-    Comercial comerciales = new Comercial("carlos",2000);
+    Comercial comerciales = new Comercial("carlos",2000,"21-5-2010");
     
     
 }
--- CarlosDelgadoDiazExamen/src/carlosdelgadodiazexamen/Comercial.java
+++ CarlosDelgadoDiazExamen/src/carlosdelgadodiazexamen/Comercial.java
@@ -12,16 +12,33 @@ package carlosdelgadodiazexamen;
 public class Comercial {
     private String nombre;
     private double sueldo;
+    private String fecha_nacimiento;
 
-    public Comercial(String nombre, double sueldo) {
+    public Comercial(String nombre, double sueldo, String fecha_nacimiento) {
         this.nombre = nombre;
         this.sueldo = sueldo;
+        this.fecha_nacimiento = fecha_nacimiento;
     }
 
     @Override
     public String toString() {
-        return "Comercial{" + "nombre=" + nombre + ", sueldo=" + sueldo + '}';
+        return "Comercial{" + "nombre=" + nombre + ", sueldo=" + sueldo + ", fecha_nacimiento=" + fecha_nacimiento + '}';
     }
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+
+
+
     
 }
